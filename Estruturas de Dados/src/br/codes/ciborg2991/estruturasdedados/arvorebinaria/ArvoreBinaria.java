c3d2b6f1ac22647f4ae55fc7ce7bceab89795c51
Cléber Macieski <cleber.macieski@gmail.com>
2017-06-16 09:14:45 -0300

    Refatorações estéticas.

--- Estruturas de Dados/src/br/codes/ciborg2991/estruturasdedados/arvorebinaria/ArvoreBinaria.java
+++ Estruturas de Dados/src/br/codes/ciborg2991/estruturasdedados/arvorebinaria/ArvoreBinaria.java
@@ -8,17 +8,17 @@ public class ArvoreBinaria {
 	}
 
 	public void exibirPreOrdem() {
-		this.exibirRecursivamente(this.raiz);
+		this.exibirPreOrdemRecursivamente(this.raiz);
 	}
 
-	private void exibirRecursivamente(Nodo percorrendo) {
+	private void exibirPreOrdemRecursivamente(Nodo percorrendo) {
 		System.out.println("Nodo: " + percorrendo.getConteudo());
 
 		if ( percorrendo.getEsquerdo() != null){
-			this.exibirRecursivamente(percorrendo.getEsquerdo());
+			this.exibirPreOrdemRecursivamente(percorrendo.getEsquerdo());
 		}
 		if ( percorrendo.getDireito() != null){
-			this.exibirRecursivamente(percorrendo.getDireito());
+			this.exibirPreOrdemRecursivamente(percorrendo.getDireito());
 		}
 	}
 
@@ -58,30 +58,6 @@ public class ArvoreBinaria {
 		catch (IllegalArgumentException e){
 			System.out.println(e.getMessage());
 		}
-		/*Nodo percorrendo = this.raiz;
-		}
-
-		if (elemento == percorrendo.getConteudo()){
-			new IllegalArgumentException("Elemento ja existente! Exceção: ");
-		}
-		else{
-			if (elemento < percorrendo.getConteudo()){
-				if (percorrendo.getEsquerdo() == null){
-					percorrendo.setEsquerdo(new Nodo(elemento));
-				}
-				else{
-					percorrendo = percorrendo.getEsquerdo();
-				}
-			}
-			else{
-				if (elemento > percorrendo.getConteudo()){
-					if (percorrendo.getDireito() == null){
-
-					}
-				}
-
-			}
-		}*/
 	}
 
 	private void inserirRecursivamente(int elemento, Nodo percorrendo) {
